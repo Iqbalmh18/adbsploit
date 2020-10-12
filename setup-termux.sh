72827a4e1bad13d3090ecfd5ab2a1c021db182d9
Iqbalmh18 <41185294+Iqbalmh18@users.noreply.github.com>
2020-10-12 16:22:18 +0700

    Delete setup-termux.sh

--- setup-termux.sh
+++
@@ -1,6 +0,0 @@
-#!~/../usr/bin/bash
-
-apt update && apt install -y python wget
-python3 -m pip install shodan rich prompt_toolkit
-wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -q && bash InstallTools.sh
-cat adbsploit.sh  > ~/../usr/bin/adbsploit && chmod +x ~/../usr/bin/adbsploit && adbsploit
