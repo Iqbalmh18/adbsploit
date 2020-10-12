f7f7f994c606a19c38de1f73e41e066f414e19ac
Iqbalmh18 <41185294+Iqbalmh18@users.noreply.github.com>
2020-10-12 16:22:08 +0700

    Delete setup-kali.sh

--- setup-kali.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-sudo apt-get update && sudo apt-get install -y android-tools-adb android-tools-fastboot && sudo apt-get install python3
-python3 -m pip install shodan rich prompt_toolkit
-cat adbsploit.sh > /usr/bin && chmod +x /usr/bin/adbsploit && adbsploit
