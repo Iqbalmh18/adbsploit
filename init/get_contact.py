f61c0fb68f7e715d7dcfd78ae634b672525e398a
Iqbalmh18 <41185294+Iqbalmh18@users.noreply.github.com>
2020-10-15 19:56:55 +0700

    update get_contact.py

--- init/get_contact.py
+++ init/get_contact.py
@@ -2,7 +2,8 @@ import os
 
 def main():
     if os.path.isfile("logs/online.log"):
-        a = open("logs/online.log","r")
+        os.system("cp -r logs/online.log on.log;sed -i 's/online//g;s/[[:space:]]//g' on.log")
+        a = open("on.log","r")
         while True:
             a1 = a.readline().strip()
             if not a:
