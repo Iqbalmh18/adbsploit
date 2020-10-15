324f90597672e12a4d791ae5e8deb91ef583c784
Iqbalmh18 <41185294+Iqbalmh18@users.noreply.github.com>
2020-10-15 18:23:25 +0700

    Update and rename all_contact.py to get_contact.py

---
+++ init/get_contact.py
@@ -0,0 +1,11 @@
+import os
+def get_contact():
+    s = open("../logs/online.log","r")
+    while True:
+        serialno = s.readline().strip()
+        if not serialno:
+            break
+        os.system("adb -s "+serialno+" shell 'content query --uri content://contacts/phones/ --projection display_name:number_key'")
+    s.close()
+
+get_contact()
