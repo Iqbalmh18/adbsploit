56d706da523c48ba5c70f1862fce212bb15cb509
Iqbalmh18 <41185294+Iqbalmh18@users.noreply.github.com>
2020-10-15 18:34:07 +0700

    update __init__.py

--- init/__init__.py
+++ init/__init__.py
@@ -162,11 +162,11 @@ class Adb():
             print(r+"[!]"+w+" device not connected")
 
     def get_contact_all(self):
-        os.system("cd init;touch 1.log 2.log cache")
+        os.system("touch 1.log 2.log cache")
         if os.path.isfile("logs/online.log"):
             os.system("python3 get_contact.py > cache 2>&1")
             os.system("cat cache | grep Row > 2.log;sed -i 's/=/ /g;s/+//g;s/display_name//g;s/number_key//g;s/Row://g;s/notes=NULL//g;s/number//g' 2.log")
-            os.system("rm -rf 1.log cache;cat 2.log > ../result/contact.txt;rm -rf 2.log;cd ..;cat result/contact.txt")
+            os.system("rm -rf 1.log cache;cat 2.log > contact.txt;rm -rf 2.log;cat contact.txt")
         else:
             exit("Invalid!")
 
