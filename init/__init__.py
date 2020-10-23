3f5cd3c6c336e3b217fe0b895c732d364adfb89a
Iqbalmh18 <41185294+Iqbalmh18@users.noreply.github.com>
2020-10-23 08:20:23 +0700

    update __init__.py

--- init/__init__.py
+++ init/__init__.py
@@ -94,7 +94,8 @@ class Adb():
                 line = address.readline().strip()
                 if not line:
                     break
-                adb.connect(line)
+               con = line+" > /dev//null 2>1"
+                adb.connect(con)
             address.close()
             os.system("adb devices | sed 's/device/online/g' > logs/cache.log")
             addres = open("logs/cache.log","r")
